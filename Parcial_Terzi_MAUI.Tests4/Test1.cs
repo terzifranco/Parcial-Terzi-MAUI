e3e05284f497894c6d8a9a98669db4c43594dab9
terzifranco <terzifrancom@gmail.com>
2026-06-30 20:59:00 -0300

    Agregar CI/CD y limpieza de proyecto de tests

---
+++ Parcial_Terzi_MAUI.Tests4/Test1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parcial_Terzi_MAUI.Tests
+{
+    [TestClass]
+    public class BasicTests
+    {
+        [TestMethod]
+        public void Test_Should_Work()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
